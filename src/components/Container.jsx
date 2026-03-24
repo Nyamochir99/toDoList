691593e40886de337e4c04aad9082309774da8a8
26bs98744 User <26bs98744@G301-CC0195.local>
2026-03-24 12:14:49 +0800

    second push

---
+++ src/components/Container.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Container = () => {
+  return (
+    <input
+      className="flex items-center w-70 h-10 rounded-md border border-[#e4e4e7] text-black text-[14px] font-normal py-4"
+      placeholder="Add a new task..."
+      type="text"
+    />
+  );
+};
